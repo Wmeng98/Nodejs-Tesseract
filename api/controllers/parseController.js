f73d77d916b076664aa48c4923e01fdb0120ee7b
Wilson Meng <w5meng@edu.uwaterloo.ca>
2019-02-02 16:14:20 -0500

    parse and retrieve upc codes

--- api/controllers/parseController.js
+++ api/controllers/parseController.js
@@ -19,6 +19,63 @@ var bModel = mongoose.model('baseModel');
 //   });
 // };
 
+// function to retrieve array of upc codes
+function getUPCCodes(text) {
+  // console.log("text >>> ");
+  // console.log(text);
+
+  var words = text.split(" ");
+  var tempUCP = "";
+  var UCPList = new Array();
+
+  // iterate through each character in the string
+  var currUCPLength = 0;
+
+  for (var i = 0; i < words.length; i += 1) {
+    // console.log("word >>> " + words[i]);
+    // only consider words where length of string >= 12
+    
+    if (words[i].includes("*")) { // assumption reached-> ACCOUNT # LINE, hence past actual items list in receipt
+      return UCPList;
+    }
+    if (words[i].length >= 12) {
+      for (var j = 0; j < words[i].length; j += 1) {
+        // If char in word is a number, add to tempUCP
+        var parsed = parseInt(words[i][j], 10)
+        if (!isNaN(parsed)) {
+          if (currUCPLength == 11) { // add last integer to ucp and restart count
+            tempUCP += words[i][j];
+
+            // compare with exceptional condition
+            if (tempUCP != "111111111111") {
+              UCPList.push(tempUCP);
+            }
+            // Reset, look for new ucp
+            currUCPLength = 0;
+            tempUCP = "";
+          } else {
+            tempUCP += words[i][j];
+            currUCPLength += 1;
+          }
+        }
+        else { // Not an integer
+          // reset counts
+          tempUCP = "";
+          currUCPLength = 0;
+        }
+      }
+    }
+  }
+
+  console.log("UCP list length: " + UCPList.length);
+  for (var i = 0; i < UCPList.length; i += 1) {
+    console.log(" >> " + UCPList[i]);
+  }
+  return UCPList;
+}
+
+
+
 exports.getImg = function(req, res) {
   bModel.find({}, function(err, img) {
     if (err) res.send(err);
@@ -40,7 +97,13 @@ exports.parseImg = function(req, res) {
 
     Tesseract.recognize(__dirname + '/img/img.jpg')
     .progress(function  (p) { console.log('progress', p)    })
-    .then(function (result) { console.log(result.text) })
+    .then(function (result) { 
+      console.log(result.text);
+      console.log(getUPCCodes(result.text));
+
+      // send 
+    })
+
 
     // // Recognize text of any language in any format
     // console.log(__dirname + '/img/img.jpg');
