b35cf940dfc696b3bb77276c0df81a7eccd949aa
Wilson Meng <w5meng@edu.uwaterloo.ca>
2019-02-02 20:11:54 -0500

    allow access control allow origin: *

--- server.js
+++ server.js
@@ -33,11 +33,11 @@ app.use(bodyParser.json());
 
 
 // CORS on ExpressJS
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // import routes
 var routes = require('./api/routes/routes');
