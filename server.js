ff6f9222c2897084de8534188bc7d404a169ab84
Wilson Meng <w5meng@edu.uwaterloo.ca>
2019-02-03 07:30:28 -0500

    server.js finalize

--- server.js
+++ server.js
@@ -146,8 +146,12 @@ app.post('/parse', (req, res) => {
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
             
-            res.send(JSON.stringify({"name": body.name}));
-
+            var jsonInput = 
+            {
+              "name" : body.name
+            }
+            res.send(JSON.stringify(jsonInput));
+            
           });
         } 
         // send 
