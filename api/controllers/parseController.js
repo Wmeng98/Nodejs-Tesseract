75e69fe917dbdf61f430bb3c67e21c2397b14086
Wilson Meng <w5meng@edu.uwaterloo.ca>
2019-02-02 23:11:57 -0500

    base64

--- api/controllers/parseController.js
+++ api/controllers/parseController.js
@@ -90,12 +90,12 @@ exports.postImg = function(req, res) {
     // create a blob object to pass the tesseract
 
     base64js.fromByteArray(strBinData);
-    base64Img.base64('img/work.jpg', function(err, data) {
+    base64Img.base64('./img/work.jpg', function(err, data) {
       if (err) res.send(err);
       console.log("image base64 img: " + data);
     })
 
-    Tesseract.recognize(__dirname + "/img/work.jpg");
+    Tesseract.recognize(__dirname + "/img/work.jpg")
     .progress(function (p) { console.log('progress', p) })
     .then(function (result) { 
       console.log(result.text);
